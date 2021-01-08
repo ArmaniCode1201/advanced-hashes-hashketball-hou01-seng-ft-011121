7b2865491685263b5acd87e046d72b183633d810
Misel Evtimov <misel.evtimov@gmail.com>
2021-01-08 19:48:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,7 +130,7 @@ def num_points_scored(name)
 
   game_hash[:home][:players].each do |player|
     if player[:player_name] == name
-      return name[:points]
+      return player[:points]
     end
 #binding.pry
   end
